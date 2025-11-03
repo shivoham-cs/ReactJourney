ec6f067fcb4ee2bd4a8d0a746dcd8689ec0ba4c0
shivoham-cs <ombhs.pro@gmail.com>
2025-11-03 13:33:11 +0530

    Added Delete and Mark Compleated functioality

--- React03/src/App.jsx
+++ React03/src/App.jsx
@@ -6,8 +6,22 @@ function App() {
 
   function addTodo() {
     if (todo.trim() === "") return;
-    setTodos([...todos, todo]);
+    setTodos([...todos, {text: todo, completed: false}]);
     setTodo("");
+  }
+
+  function toggleCompleted (indexToggle) {
+    const updatedTodos = todos.map((item, index) => {
+      if(index === indexToggle) {
+        return { ...item, completed: !item.completed}
+      }
+      return item;
+    })
+    setTodos(updatedTodos);
+  }
+
+  function deleteTodo(indexToDelete) {
+    setTodos(todos.filter((_, index) => index !== indexToDelete));
   }
 
   return (
@@ -21,7 +35,27 @@ function App() {
       <button onClick={addTodo}>Add</button>
       <ul>
         {todos.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li key={index}>
+            <button
+              onClick={() => toggleCompleted(index)}
+              style={{
+                backgroundColor: item.completed ? "green" : "lightgray",
+                border: item.completed ? "1px solid darkgreen" : "1px solid black",
+                padding: "0 6px",
+                borderRadius: "50%",
+                cursor: "pointer",
+              }}
+            >
+              &nbsp;
+            </button>
+            <span style={{textDecoration: item.completed ? "line-through" : "none"}}>
+                {item.text}
+            </span>
+            {""}
+            <button onClick={() => deleteTodo(index)}>
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </>
