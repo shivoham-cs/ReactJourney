aa2bde468952049627ffa1eb8e0ab94086be7b15
shivoham-cs <ombhs.pro@gmail.com>
2025-11-05 20:52:22 +0530

    Used useEffect Hook. Partially compleated.

---
+++ search-filter/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react'
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await res.json();
+      setUsers(data);
+      setFilteredUsers(data);
+    }
+    fetchUsers();
+  }, []);
+
+  useEffect(() => {
+    const results = users.filter((user) =>
+      user.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredUsers(results);
+  }, [search, users]);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>🔍 Live User Search</h2>
+      <input
+        type="text"
+        placeholder="Search user..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <ul>
+        {filteredUsers.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App
